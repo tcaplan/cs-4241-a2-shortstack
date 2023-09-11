6a0a9555e24da44b13f385e392348f7e0d52017b
da-munchies24 <donbothermeatall16@gmail.com>
2023-09-10 23:39:37 -0400

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,80 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+const submit = async function (event) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault()
-  
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
-  
-try{
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
-  })
-  const text = await response.json()
+  event.preventDefault();
 
-  console.log( 'text:', text )
-  const list= document.createElement('ul')
-  text.array.forEach(d => {
-    const item = document.createElement('li')
-    item.innerHTML=` <b>model</b> : ${d.model}, <b>mpg</b>: ${d.mpg}, <b>year</b>: ${d.year}`
-    list.appendChild(item)
-  })
+  let input = document.querySelectorAll("#Email,#Name,#Birth"),
+    json = {
+      Email: input[0].value,
+      Name: input[1].value,
+      Birth: input[2].value,
+    };
+  //this sends a request to the server
+  let response = await fetch("/submit", {
+    method: "POST",
+    body: JSON.stringify(json),
+  });
+  const text = await response.json();
+  document.querySelector("#fade").style.display = "block";
+  setTimeout(function () {
+    document.querySelector("#fade").style.display = "none";
+  }, 2000);
+};
 
- // list.innerHTML=data
-  //map is going to return a new array which we are going through using .foreach 
-  // which will return the same array with some modifications to it. 
-  /*.map(d=>d.model)
-  .map(d=> d[0].toUpperCase(0) + d.slice(1))
-  .foreach(d=> {
-    const li= document.createElement('li')
-    list.appendChild(li)
-  })
-*/
-  document.body.appendChild(list)
+function age(birth) {
+  let date = new Date(birth);
+  let diff = Date.now() - date.getTime();
+  let date1 = new Date(diff);
+  let age = Math.abs(date1.getUTCFullYear() - 1970);
+  return age;
 }
-catch(err){
-  console.log(err.message);
-  document.getElementsByClassName("body").innerHTML= '<p> Request Failed </p>';
-}}
 
-window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
-}
+const display = async function (event) {
+  event.preventDefault();
+
+  let response = await fetch("/display", {
+    method: "Get",
+  });
+  const text = await response.json();
+
+  const list = document.createElement("table");
+  const header = document.createElement("tr");
+  header.innerHTML = `<th>Email</th> <th>Name</th> <th>Birth</th> <th>Age</th> <th>Delete</th>`;
+  list.appendChild(header);
+  let i = 0;
+  text.forEach((d) => {
+    i++;
+    const year = age(d.Birth);
+    const item = document.createElement("tr");
+    item.innerHTML = `
+    <td>${d.Email}</td> 
+    <td>${d.Name}</td>
+    <td>${d.Birth}</td>
+    <td>${year} years old</td>
+    <td>row:${i} <input type="checkbox" name=${i}/></td>
+    `;
+    list.appendChild(item);
+  });
+
+  document.body.appendChild(list);
+  /*  const ut=document.createElement("div");
+  ut.setAttribute("id","delbtn");
+  ut.innerHTML=`<button>Delete</button>`;*/
+
+  document.querySelector("#clear").addEventListener("click", function () {
+    document.body.removeChild(list);
+    // document.body.remove(ut);
+  });
+};
+
+
+window.onload = function () {
+  const button1 = document.querySelector("#submit");
+  const button2 = document.querySelector("#display");
+  button1.onclick = submit;
+  button2.onclick = display;
+};
